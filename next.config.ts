f913e58c2d7268b3e9020821394ea576dc7e61a5
tawiah <tawiahin4k@gmail.com>
2025-01-04 18:26:32 +0000

    first commit

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,11 @@
+/** @format */
+
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  output: "export",
+  images: { unoptimized: true },
 };
 
 export default nextConfig;
